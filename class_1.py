15e63460779d6eca0fed5b51e62fde2f94b03430
pumpkin-nbc <a760329881@qq.com>
2020-02-09 10:28:37 +0800

    Create class_1.py

---
+++ class_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 15:54:45 2020
+
+@author: Mr.Nan
+"""
+
+print('------------------------')
+temp=input("please write a number")
+guess=temp
+if guess == 8:
+    print("you are right")
+else:
+    print(" it's not right ")
+print("stop")
