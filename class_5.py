d8d72a0a5282af8220d4bcde68de6aa3e64530d4
pumpkin-nbc <a760329881@qq.com>
2020-02-16 16:09:00 +0800

    Create class_5.py

---
+++ class_5.py
@@ -0,0 +1,18 @@
+#实现_函数（def）
+def MyFirstFunction():
+    print('这是我创建的第一个函数')
+    print('it\'s interset')
+    print('123456789')
+
+MyFirstFunction()
+
+def MySecoundFunctuon(name):
+    print(name + '我爱你！')
+
+MySecoundFunctuon('aa')
+
+def add(num1,num2):
+    return (num1 +num2)
+
+
+print(add(2,3))
