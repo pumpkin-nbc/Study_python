2293c65d27027af56d926691fc896a7d979dd384
pumpkin-nbc <a760329881@qq.com>
2020-02-18 20:25:36 +0800

    Create class_9.py

---
+++ class_9.py
@@ -0,0 +1,11 @@
+def hanoi(n,x,y,z):
+    if n == 1:
+        print(x,'-->',z)
+    else:
+        hanoi(n-1,x,z,y)
+        print(x,'-->',z)
+        hanoi(n-1,y,x,z)
+
+n = int(input('number'))
+hanoi(n,'X','Y','Z')
+
